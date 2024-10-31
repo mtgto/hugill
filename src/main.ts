2c9127d043423907804a30e181ae871530e3c13b
mtgto <hogerappa@gmail.com>
2024-10-31 21:16:24 +0900

    git hook using lefthook

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { mount } from "svelte";
 import App from "./App.svelte";
 
 const app = mount(App, {
-  // biome-ignore lint: lint/style/noNonNullAssertion
-  target: document.getElementById("app")!,
+	// biome-ignore lint: lint/style/noNonNullAssertion
+	target: document.getElementById("app")!,
 });
 
 export default app;
