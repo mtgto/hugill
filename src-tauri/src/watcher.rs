4ea9fde26fb9d45e70c39ae967dfd15e9728cd6e
mtgto <hogerappa@gmail.com>
2024-11-26 22:42:35 +0900

    polling

--- src-tauri/src/watcher.rs
+++ src-tauri/src/watcher.rs
@@ -1,14 +1,11 @@
 use tauri::{AppHandle, Emitter, EventTarget};
 
 pub fn start(handle: AppHandle) -> Result<(), Box<dyn std::error::Error>> {
-    std::thread::spawn(move || {
-        handle.emit_to(EventTarget::app(), "watcher", 0);
-        /*
-        loop {
-            thread::sleep(std::time::Duration::from_secs(5));
-            app.emit_to(EventTarget::app(), "watcher", []).expect("failed to emit watcher event");
-        }
-        */
+    std::thread::spawn(move || loop {
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        let _ = handle
+            .emit_to(EventTarget::app(), "watcher", 0)
+            .expect("failed to emit watcher event");
     });
     Ok(())
 }
