01473c5f2774fac7f121d9b90d71722b31d668d3
mtgto <hogerappa@gmail.com>
2024-11-30 19:26:30 +0900

    Refactor

--- src-tauri/src/watcher.rs
+++ src-tauri/src/watcher.rs
@@ -8,19 +8,16 @@ use tauri::{AppHandle, Emitter, EventTarget};
 use tokio::time;
 
 pub fn start(handle: AppHandle) -> Result<(), Box<dyn std::error::Error>> {
-    let (current_context, client) = tauri::async_runtime::block_on(async move {
-        let kubeconfig = Kubeconfig::read()?;
-        let current_context = kubeconfig
-            .current_context
-            .clone()
-            .ok_or("no current context")?;
-        let config =
-            Config::from_custom_kubeconfig(kubeconfig, &KubeConfigOptions::default()).await?;
-        let client = Client::try_from(config)?;
-        return Ok::<(std::string::String, Client), Box<dyn std::error::Error>>((
-            current_context,
-            client,
-        ));
+    let kubeconfig = Kubeconfig::read()?;
+    let current_context = kubeconfig
+        .current_context
+        .clone()
+        .ok_or("no current context")?;
+    let client = tauri::async_runtime::block_on(async move {
+        let config = Config::from_custom_kubeconfig(kubeconfig, &KubeConfigOptions::default())
+            .await
+            .map_err(|_| "failed to load kubeconfig")?;
+        return Client::try_from(config).map_err(|_| "failed to load config for k8s");
     })?;
     println!("default context: {}", current_context);
     tauri::async_runtime::spawn(async move {
